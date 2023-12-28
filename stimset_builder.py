aaf5e81ab33ef48eb9e516b95dd3ac44eb2b11be
Travis Hage <travishage@gmail.com>
2023-12-28 14:42:06 -0800

    hexagon grids and alt route planner to avoid n+2 adjacency

--- stimset_builder.py
+++ stimset_builder.py
@@ -90,6 +90,17 @@ def distances_all_steps(sequence_order, coms):
 		distances[step] = d
 	return distances
 
+def distances_n_steps(sequence_order, coms, n_steps=2):
+
+
+    n_stims = len(sequence_order)
+    distances = np.zeros(n_stims - n_steps)
+    for step in np.arange(0, (n_stims-n_steps)):
+        i, j = sequence_order[step], sequence_order[step + n_steps]
+        d = distance_between_coms(i, j, coms)
+        distances[step] = d
+    return distances
+
 ##https://stackoverflow.com/questions/25585401/travelling-salesman-in-scipy
 ##suggested solution to traveling salesman problem, aiming to minimize route distance
 def two_opt(cities,improvement_threshold): # 2-opt Algorithm adapted from https://en.wikipedia.org/wiki/2-opt
@@ -122,8 +133,6 @@ def two_opt_long(coms,improvement_threshold, r_seed = 42): # 2-opt Algorithm ada
 	random.shuffle(route) ##starting with 0...n seemed to result in quandrant-ish patterns
 	improvement_factor = 1 # Initialize the improvement factor.
 	best_distance = path_distance(route,coms) # Calculate the distance of the initial path.
-	counter=0
-	min_dist_threshold = 200
 	while improvement_factor > improvement_threshold: # If the route is still improving, keep going!
 		distance_to_beat = best_distance # Record the distance at the beginning of the loop.
 		for swap_first in range(1,len(route)-2): # From each city except the first and last,
@@ -131,14 +140,77 @@ def two_opt_long(coms,improvement_threshold, r_seed = 42): # 2-opt Algorithm ada
 				new_route = two_opt_swap(route,swap_first,swap_last) # try reversing the order of these coms
 				new_distance = path_distance(new_route,coms) # and check the total distance with this modification.
 				min_dist = np.min(distances_all_steps(new_route, coms))
-				counter+=1
 				if new_distance > best_distance: # If the path distance is an improvement,
 					route = new_route # make this the accepted best route
 					best_distance = new_distance # and update the distance corresponding to this route.
 		improvement_factor = best_distance/distance_to_beat -1 # Calculate how much the route has improved.
 	return route # When the route is no longer improving substantially, stop searching and return the route.
 
+def two_opt_one_two_step(coms,improvement_threshold, r_seed = 42): # 2-opt Algorithm adapted from https://en.wikipedia.org/wiki/2-opt
+	route = np.arange(coms.shape[0]) # Make an array of row numbers corresponding to coms.
+	random.seed(r_seed)
+	random.shuffle(route) ##starting with 0...n seemed to result in quandrant-ish patterns
+	improvement_factor = 1 # Initialize the improvement factor.
+	best_distance = np.min(distances_all_steps(route, coms)) + np.min(distances_n_steps(route, coms, n_steps=2)) # consider minimum 1-step and 2-step sizes
+	while improvement_factor > improvement_threshold: # If the route is still improving, keep going!
+		distance_to_beat = best_distance # Record the distance at the beginning of the loop.
+		for swap_first in range(1,len(route)-2): # From each city except the first and last,
+			for swap_last in range(swap_first+1,len(route)): # to each of the coms following,
+				new_route = two_opt_swap(route,swap_first,swap_last) # try reversing the order of these coms
+				new_distance = np.min(distances_all_steps(new_route, coms)) + np.min(distances_n_steps(new_route, coms, n_steps=2)) # and check the total distance with this modification.
+				if new_distance > best_distance: # If the path distance is an improvement,
+					route = new_route # make this the accepted best route
+					best_distance = new_distance # and update the distance corresponding to this route.
+		improvement_factor = best_distance/distance_to_beat -1 # Calculate how much the route has improved.
+	return route # When the route is no longer improving substantially, stop searching and return the route.
 
+def polygon(sides, radius=1, rotation=0, translation=None):
+	one_segment = math.pi * 2 / sides
+	points = [
+		(math.sin(one_segment * i + rotation) * radius,
+		math.cos(one_segment * i + rotation) * radius)
+		for i in range(sides)]
+
+	if translation:
+		points = [[sum(pair) for pair in zip(point, translation)]
+		for point in points]
+	return points
+
+def draw_hex(n_cols, n_rows, i_col, i_row, img_w = 1024, img_h = 1024, plot = False):
+	left_margin = 20 
+	right_margin =120 
+	upper_margin =120 
+	lower_margin = 397
+	sub_w = img_w - left_margin - right_margin
+	sub_h = img_h - upper_margin - lower_margin
+	poly_r = math.floor(sub_w/n_cols)/2
+	one_segment = math.pi * 2 / 6
+	y_shift = (math.cos(one_segment*1)*poly_r)+poly_r+1 ##+1 to err on side of avoid overlap
+	x_shift = (math.sin(one_segment*1)*poly_r)*2+1
+	x0 = left_margin + i_col*x_shift + x_shift
+	y0 = upper_margin + i_row*y_shift +y_shift
+	if i_row%2==0:
+		x0+=x_shift/2
+	points = polygon(6, radius = poly_r, translation = [x0, y0])
+	points = np.array(points).astype(np.int32).reshape((-1, 1, 2))
+	stim = cv2.fillPoly(img = np.zeros((img_w, img_h), 
+		dtype = np.uint8),
+		pts = [points],
+		color = 255)
+	if plot:
+		plt.imshow(stim)
+	return stim
+
+def draw_all_grid_hex(n_cols, n_rows, img_w=1024, img_h=1024):
+	n_stims = n_cols * n_rows
+	grid_stims = np.zeros((img_w, img_h, n_stims), dtype = np.uint8)
+	grid_i = 0
+	for ic in np.arange(n_cols):
+		for ir in np.arange(n_rows):
+			stim = draw_hex(n_cols, n_rows, ic, ir)
+			grid_stims[:, :, grid_i] = stim
+			grid_i += 1
+	return grid_stims
 
 class StimSequenceSet():
     def __init__(self, image_sequence, name, sequence_dict):
@@ -147,6 +219,7 @@ class StimSequenceSet():
         self.n_patterns = image_sequence.shape[2]
         self.sequence_dict = sequence_dict
         self.time_created = datetime.datetime.now()
+        self.image_shape = image_sequence.shape[:-1]
 
     def get_single_image(self, i):
         return self.image_sequence[:,:,i]
